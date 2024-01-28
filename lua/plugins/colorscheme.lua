fdf3a35d617b15f7bfa2e2871708c0e703336343
c-tollison <tollisoncarson@gmail.com>
2024-01-28 12:48:55 -0500

    One dark color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  "navarasu/onedark.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      style = "warm",
+      transparent = true,
+      term_colors = true,
+    }
+  end,
+}
